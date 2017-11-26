27b47a8727fc79f74203bd78929f6682084d4dbf
mattreyes7 <mattreyes615@gmail.com>
2017-11-25 23:50:39 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing")
+})
+
+app.listen(3000, function(){
+  console.log("listening");
+});
